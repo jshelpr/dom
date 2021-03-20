cc0a7446f8d72e54a8fd0a5636db570de5dd7e09
Haneen Mahdin <haneenmahdin@gmail.com>
2021-03-20 11:33:35 +0530

    Added createElement

---
+++ index.js
@@ -0,0 +1,6 @@
+// index file
+/** @license */
+/**@author Haneen Mahdin */
+export { replaceId } from "./lib/replaceId";
+export { replaceClass } from "./lib/replaceClass";
+export { elementIsPresentClass, elementIsPresent, elementIsPresentTagName, elementIsPresentName };
