83a234b568df752215c6a93a7b08dcb56e6cccaf
Haneen Mahdin <haneenmahdin@gmail.com>
2021-03-20 22:05:09 +0530

    Fixed the functions undefined issue

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // index file
 /** @license */
 /**@author Haneen Mahdin */
-export { replaceId } from "./lib/replaceId";
-export { replaceClass } from "./lib/replaceClass";
-export { elementIsPresentClass, elementIsPresent, elementIsPresentTagName, elementIsPresentName };
+import replaceId from "./lib/document/replaceId.js";
+import { replaceClass } from "./lib/document/replaceClass.js";
+import { elementIsPresentClass, elementIsPresent, elementIsPresentTagName, elementIsPresentName } from "./lib/document/elementIsPresent.js";
+
+export { replaceClass, replaceId };
+export { elementIsPresent, elementIsPresentClass, elementIsPresentName, elementIsPresentTagName };
