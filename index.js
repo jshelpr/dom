bdbd93db844230705355dcb8e8c4936e3c230b52
Haneen Mahdin <haneenmahdin@gmail.com>
2021-03-22 10:28:24 +0530

    added webpack for module bundling

--- index.js
+++
@@ -1,9 +0,0 @@
-// index file
-/** @license */
-/**@author Haneen Mahdin */
-import replaceId from "./lib/document/replaceId.js";
-import { replaceClass } from "./lib/document/replaceClass.js";
-import { elementIsPresentClass, elementIsPresent, elementIsPresentTagName, elementIsPresentName } from "./lib/document/elementIsPresent.js";
-
-export { replaceClass, replaceId };
-export { elementIsPresent, elementIsPresentClass, elementIsPresentName, elementIsPresentTagName };
